087cb4ef2849475beff86a69ee06b67cb876ca3c
katjaalcock <114230664+katjaalcock@users.noreply.github.com>
2024-05-08 16:28:57 +0100

    dfe main updates

--- app/routes.js
+++ app/routes.js
@@ -8,12 +8,11 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
-router.post('/v1/eligibility1-answers', function(request, response) {
+router.post('/eligibility-route', function(request, response) {
 
-    var elig1 = request.session.data['eligibility1']
-    if (elig1 == "X"){
+var course = request.session.data['course-type']
+if (course){
+    request.session.data['question-answered'] = 'yes'
+}
         response.redirect("/layouts/v1/eligibility-check-answers")
-    } else if (elig1 == "Y"){
-        response.redirect("/layouts/v1/eligibility-check-answers")
-    }
-})
+    })
