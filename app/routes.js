b89119b419d8bd5878c8bcee05ef40042cb19cfb
katjaalcock <114230664+katjaalcock@users.noreply.github.com>
2024-05-08 14:13:18 +0100

    Routes

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,13 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/v1/eligibility1-answers', function(request, response) {
+
+    var elig1 = request.session.data['eligibility1']
+    if (elig1 == "X"){
+        response.redirect("/layouts/v1/eligibility-check-answers")
+    } else if (elig1 == "Y"){
+        response.redirect("/layouts/v1/eligibility-check-answers")
+    }
+})
