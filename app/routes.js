f959ad503bce3923964eeed42746baa180224902
katjaalcock <114230664+katjaalcock@users.noreply.github.com>
2024-06-24 12:39:13 +0100

    Update eligibility questions

--- app/routes.js
+++ app/routes.js
@@ -54,22 +54,34 @@ router.post('/scope', function (req, res) {
     var scope = req.session.data['scope']
     if (scope == 'yes'){
         // Send user to next page
-        res.redirect('/ao/v2/check2')
+        res.redirect('/ao/v2/check5')
       } else {
         // Send user to ineligible page
-        res.redirect('/ao/v2/ineligible1')
+        res.redirect('/ao/v2/ineligible-scope')
       }
     
+    })
+
+router.post('/scopetq', function (req, res) {
+    var scopetq = req.session.data['scopetq']
+    if (scopetq == 'yes'){
+         // Send user to next page
+        res.redirect('/ao/v2/application-overview-tq')
+        } else {
+        // Send user to ineligible page
+        res.redirect('/ao/v2/ineligible-scope')
+        }
+        
     })
 
 router.post('/teachdate', function (req, res) {
      var teachdate = req.session.data['teachdate']
      if (teachdate == 'yes'){
          // Send user to next page
-         res.redirect('/ao/v2/check3')
+         res.redirect('/ao/v2/application-overview-aaq')
       } else {
          // Send user to ineligible page
-        res.redirect('/ao/v2/ineligible1')
+        res.redirect('/ao/v2/ineligible-teachdate')
       }
         
    })
@@ -78,10 +90,34 @@ router.post('/qualtype', function (req, res) {
         var qualtype = req.session.data['qualtype']
         if (qualtype == 'aaq'){
          // Send user to next page
-         res.redirect('/ao/v2/application-overview-aaq')
+         res.redirect('/ao/v2/check4-aaq')
             } else {
           // Send user to ineligible page
-         res.redirect('/ao/v2/application-overview-tq')
+         res.redirect('/ao/v2/check4-tq')
              }
             
  })
+
+router.post('/england', function (req, res) {
+    var england = req.session.data['england']
+    if (england == 'yes'){
+     // Send user to next page
+     res.redirect('/ao/v2/check2')
+        } else {
+      // Send user to ineligible page
+     res.redirect('/ao/v2/ineligible-england')
+         }
+        
+})
+
+router.post('/national', function (req, res) {
+    var national = req.session.data['national']
+    if (national == 'yes'){
+     // Send user to next page
+     res.redirect('/ao/v2/check3')
+        } else {
+      // Send user to ineligible page
+     res.redirect('/ao/v2/ineligible-national')
+         }
+        
+})
