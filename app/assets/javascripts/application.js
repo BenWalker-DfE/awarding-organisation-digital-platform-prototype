ca2481cf2a4d697c088cc323d59f9e6439c9f709
katjaalcock <114230664+katjaalcock@users.noreply.github.com>
2024-07-11 19:32:51 +0100

    Funding section

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,51 +4,5 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  $(function() {
-
-
-    $('#id_route').on('change', function(e) {
-
-        if ($(this).val() == agriculture) {
-
-            var arr = ['1', '2'];
-            $('#id_standard option').each(function(i) {
-                if ($.inArray($(this).attr('value'), arr) == -1) {
-                    $(this).css('display', 'none');
-                } else {
-                    $(this).css('display', 'inline-block');
-                }
-            });
-        } else if ($(this).val() == business) {
-
-            var arr = ['3', '4'];
-            $('#id_standard option').each(function(i) {
-                if ($.inArray($(this).attr('value'), arr) == -1) {
-                    $(this).css('display', 'none');
-                } else {
-                    $(this).css('display', 'inline-block');
-                }
-            });
-        } else if ($(this).val() == care) {
-
-            var arr = ['5', '6'];
-            $('#id_choices option').each(function(i) {
-                if ($.inArray($(this).attr('value'), arr) == -1) {
-                    $(this).css('display', 'none');
-                } else {
-                    $(this).css('display', 'inline-block');
-                }
-            });
-        } else {
-
-            $('#id_choices option').each(function(i) {
-                $(this).css('display', 'inline-block');
-            });
-
-        }
-
-    })
-
-
-});
+  
 })
